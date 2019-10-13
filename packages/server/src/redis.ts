35c5c25f8ef9235f8f3a26c158f0a36cc6faeca6
SergeiMeza <sergeimeza@gmail.com>
2019-10-14 03:54:49 +0900

    autocommit

--- packages/server/src/redis.ts
+++ packages/server/src/redis.ts
@@ -1,8 +1,8 @@
 import * as Redis from 'ioredis'
 
 export const redis = new Redis(
-  undefined,
-  process.env.NODE_ENV === 'local' ? 'host.docker.internal' : undefined,
+  process.env.NODE_ENV === 'dev' ? 6379 : undefined,
+  process.env.NODE_ENV === 'dev' ? 'host.docker.internal' : undefined,
 )
 
 redis.on('connect', () => console.log('Redis connected'))
